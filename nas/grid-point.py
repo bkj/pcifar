c24be2973abc54192717053bf967b70706e3578b
bjohnson <ben@gophronesis.com>
2017-09-06 18:54:09 -0400

    ...

--- nas/grid-point.py
+++ nas/grid-point.py
@@ -65,8 +65,11 @@ config.update({'args' : vars(args)})
 print >> sys.stderr, json.dumps(config)
 
 config_path = os.path.join(file_prefix, 'configs', config['model_name'])
-open(config_path, 'w').write(json.dumps(config))
+hist_path = os.path.join(file_prefix, 'hists', config['model_name'])
+model_path = os.path.join(file_prefix, 'states', config['model_name'])
 
+open(config_path, 'w').write(json.dumps(config))
+histfile = open(hist_path, 'w')
 
 # --
 # Training helpers
@@ -78,8 +81,6 @@ def train_epoch(net, loader, opt, epoch, n_train_batches=ds['n_train_batches']):
     total = 0
     gen = tqdm(enumerate(loader), total=len(loader))
     for batch_idx, (data, targets) in gen:
-        
-        # !! Is this slow?
         LRSchedule.set_lr(opt, lr_schedule(epoch + batch_idx / n_train_batches))
         
         data, targets = Variable(data.cuda()), Variable(targets.cuda())
@@ -90,7 +91,6 @@ def train_epoch(net, loader, opt, epoch, n_train_batches=ds['n_train_batches']):
         total += targets.size(0)
         correct += (predicted == targets.data).cpu().sum()
         
-        # !! Is this slow?
         curr_acc = correct / total
         curr_loss = all_loss / (batch_idx + 1)
         gen.set_postfix({'curr_acc' : curr_acc, 'curr_loss' : curr_loss})
@@ -111,21 +111,17 @@ def eval(net, epoch, loader):
     for batch_idx, (data, targets) in gen:
         
         data, targets = Variable(data.cuda(), volatile=True), Variable(targets.cuda())
-        
         outputs = net(data)
         loss = F.cross_entropy(outputs, targets).data[0]
-        
         all_loss += loss
         
         predicted = outputs.data.max(1)[1]
         total += targets.size(0)
         correct += (predicted == targets.data).cpu().sum()
         
-        # !! Is this slow?
         curr_acc = correct / total
         curr_loss = all_loss / (batch_idx + 1)
         gen.set_postfix({'curr_acc' : curr_acc, 'curr_loss' : curr_loss})
-
     
     return curr_acc, curr_loss
 
@@ -135,9 +131,6 @@ def eval(net, epoch, loader):
 net = RNet(config['op_keys'], config['red_op_keys']).cuda() 
 opt = optim.SGD(net.parameters(), lr=lr_schedule(0.0), momentum=0.9, weight_decay=5e-4)
 print >> sys.stderr, net
-
-hist_path = os.path.join(file_prefix, 'hists', config['model_name'])
-hist = open(hist_path, 'w')
 
 for epoch in range(args.epochs):
     print >> sys.stderr, "Epoch=%d" % epoch
@@ -152,7 +145,7 @@ for epoch in range(args.epochs):
     test_acc, test_loss = eval(net, epoch, ds['test_loader'])
     
     # Log
-    hist.write(json.dumps({
+    histfile.write(json.dumps({
         'epoch'      : epoch, 
         'train_acc'  : train_acc, 
         'train_loss' : train_loss,
@@ -161,11 +154,11 @@ for epoch in range(args.epochs):
         'test_acc'   : test_acc,
         'test_loss'  : test_loss,
     }) + '\n')
-    hist.flush()
+    histfile.flush()
+
+histfile.close()
 
 # --
 # Save final model
 
-model_path = os.path.join(file_prefix, 'states', config['model_name'])
-print >> sys.stderr, 'saving model: %s' % model_path
 torch.save(net.state_dict(), model_path)
