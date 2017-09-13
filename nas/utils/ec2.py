1c9b1f2f73fce4bcc60a7929616cf6f3eccdbf42
bjohnson <ben@gophronesis.com>
2017-09-13 13:39:23 -0400

    ...

--- nas/utils/ec2.py
+++ nas/utils/ec2.py
@@ -3,20 +3,21 @@
 """
     ec2.py
     
-    Launch EC2 instance and run training
+    Launch EC2 instance for running training
 """
 
+import sys
+import uuid
 import json
 import boto3
 import base64
-import uuid
 
 # --
 # Helpers
 
 client = boto3.client('ec2')
 
-def make_cmd(configs):
+def make_cmd(configs, terminate=False):
     cmd = b"""#!/bin/bash
 
 echo '
@@ -38,12 +39,15 @@ sudo git pull
 mkdir -p logs
 python ./grid-point.py \
     --config-b64 $CONFIG \
-    --epochs 2 \
+    --epochs 20 \
+    --lr-schedule linear \
+    --run ec2-0 \
     --run ec2 2>./logs/$MID.log || true
 
 export AWS_SHARED_CREDENTIALS_FILE=/home/ubuntu/.aws/credentials
 find results/ | fgrep $MID | xargs -I {} aws s3 cp {} s3://cfld-nas/{}
 aws s3 cp ./logs/$MID.log s3://cfld-nas/logs/$MID.log
+
 ' >> /home/ubuntu/run.sh
 
 chmod +x /home/ubuntu/run.sh
@@ -55,10 +59,16 @@ chmod +x /home/ubuntu/run.sh
 # su -c '/home/ubuntu/run.sh %s %s' -m ubuntu
         """ % (config['model_name'], base64.b64encode(json.dumps(config)))
     
+    if terminate:
+        cmd += """
+            echo "terminating..."
+            sudo halt
+        """
+    
     return cmd, base64.b64encode(cmd)
 
 
-def launch_spot(cmd_b64, spot_price=0.20):
+def launch_spot(cmd_b64, spot_price=0.25):
     return client.request_spot_instances(
         DryRun=False,
         SpotPrice=str(spot_price),
@@ -79,14 +89,15 @@ def launch_spot(cmd_b64, spot_price=0.20):
 # --
 # Run
 
-# Define job
-mid = str(uuid.uuid1())
-config = {
-    "op_keys": ["double_bnconv_3", "identity", "add"], 
-    "red_op_keys": ["conv_1", "double_bnconv_3", "add"],
-    "model_name" : mid,
-}
-
-cmd_clear, cmd_b64 = make_cmd([config])
-print cmd_clear
-launch_spot(cmd_b64)
+if __name__ == "__main__":
+    mid = str(uuid.uuid1())
+    config = {
+        "op_keys": ["double_bnconv_3", "identity", "add"], 
+        "red_op_keys": ["conv_1", "double_bnconv_3", "add"],
+        "model_name" : mid,
+    }
+    
+    cmd_clear, cmd_b64 = make_cmd([config])
+    print >> sys.stderr, cmd_clear
+    res = launch_spot(cmd_b64)
+    print res['SpotInstanceRequests'][0]['SpotInstanceRequestId']
